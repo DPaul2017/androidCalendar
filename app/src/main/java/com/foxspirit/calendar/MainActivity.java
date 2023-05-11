04f20c82ace0c099e65c3bf6935247157c1341f1
DPaul2017 <deefeed2010@yandex.ru>
2023-05-11 12:15:23 +0300

    Little changes in code for better understanding

--- app/src/main/java/com/foxspirit/calendar/MainActivity.java
+++ app/src/main/java/com/foxspirit/calendar/MainActivity.java
@@ -2,22 +2,24 @@ package com.foxspirit.calendar;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.GridView;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.Calendar;
-
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private TextView calendarDisplayWeekDay;
-    private Button calendarGoToTodayButton;
 
+    ImageView calendarPreviewButton;
+    ImageView calendarNextButton;
+    TextView calendarDisplayDate;
+    TextView calendarDisplayMonth;
+    TextView calendarDisplayYear;
+    TextView calendarDisplayWeekDay;
+    ImageView calendarGoToTodayButton;
+    GridView calendarGrid;
+    TextView tv;
     MyCalendar mc;
 
     @Override
@@ -26,32 +28,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_calendar);
 
 
-        ImageView calendarPreviewButton = (ImageView) findViewById(R.id.calendar_prev_button);
-        ImageView calendarNextButton = (ImageView) findViewById(R.id.calendar_next_button);
-        TextView calendarDisplayDate = (TextView) findViewById(R.id.date_display_date);
-        TextView calendarDisplayMonth = (TextView) findViewById(R.id.date_display_month);
-        TextView calendarDisplayYear = (TextView) findViewById(R.id.date_display_year);
-        calendarDisplayWeekDay = (TextView) findViewById(R.id.date_display_day);
-        calendarGoToTodayButton = (Button) findViewById(R.id.date_display_today);
-        GridView calendarGrid = (GridView) findViewById(R.id.calendar_grid);
-
-        TextView tv = findViewById(R.id.textView);
-        calendarPreviewButton.setOnClickListener(this);
-        calendarNextButton.setOnClickListener(this);
-        calendarDisplayDate.setOnClickListener(this);
-        calendarDisplayMonth.setOnClickListener(this);
-        calendarDisplayYear.setOnClickListener(this);
-        calendarDisplayWeekDay.setOnClickListener(this);
-        calendarGoToTodayButton.setOnClickListener(this);
-        calendarGrid.setOnItemClickListener((parent, view, position, id) -> {
-            Integer date = (Integer) parent.getItemAtPosition(position);
-            String text = ((TextView) view).getText().toString();
-            mc.setDate(date);
-            mc.updateFields();
-        });
+        initVars();
+        setListeners();
 
         mc = new MyCalendar(calendarDisplayDate, calendarDisplayWeekDay, calendarDisplayMonth, calendarDisplayYear, calendarGrid, tv);
         mc.updateFields();
+
     }
 
     @Override
@@ -69,10 +51,46 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             default:
                 viewToastMessage("Not set");
+                break;
         }
     }
 
     private void viewToastMessage(String message) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    private void initVars() {
+        calendarPreviewButton = (ImageView) findViewById(R.id.calendar_prev_button);
+        calendarNextButton = (ImageView) findViewById(R.id.calendar_next_button);
+        calendarDisplayDate = (TextView) findViewById(R.id.date_display_date);
+        calendarDisplayMonth = (TextView) findViewById(R.id.date_display_month);
+        calendarDisplayYear = (TextView) findViewById(R.id.date_display_year);
+        calendarDisplayWeekDay = (TextView) findViewById(R.id.date_display_day);
+        calendarGoToTodayButton = (ImageView) findViewById(R.id.date_display_today);
+        calendarGrid = (GridView) findViewById(R.id.calendar_grid);
+        tv = findViewById(R.id.textView);
+    }
+
+    private void setListeners() {
+        calendarPreviewButton.setOnClickListener(this);
+        calendarNextButton.setOnClickListener(this);
+        calendarDisplayDate.setOnClickListener(this);
+        calendarDisplayMonth.setOnClickListener(this);
+        calendarDisplayYear.setOnClickListener(this);
+        calendarDisplayWeekDay.setOnClickListener(this);
+        calendarGoToTodayButton.setOnClickListener(this);
+        calendarGrid.setOnItemClickListener((parent, view, position, id) -> {
+            Integer date = (Integer) parent.getItemAtPosition(position);
+            String text = ((TextView) view).getText().toString();
+            mc.setDate(date);
+            mc.updateFields();
+        });
+    }
+
+
 }
